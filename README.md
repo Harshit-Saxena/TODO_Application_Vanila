74b45d553b1e29c604052d30050b1a0caf967b3e
Harshit Saxena <18752758+Harshit-Saxena@users.noreply.github.com>
2021-09-12 23:31:20 +0530

    Day-2 writing function whole DS about tobe strike through

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TODO_Application_Vanila
 This is the application 'personal project' made in vanila JS
+Things we need to build these -->
+FrontEnd dev -- css to Strike through
+                Basic Structure of div 
+
+BackEnd dev -- onclick func for edit , del, add task
+               delete the task from the stack and count-- 
+               edit the task inline and the refresh the whole stack 
+               mark the task to be done.
+               
--- TODO.js
+++ TODO.js
@@ -1,8 +1,22 @@
+var taskStrike = 'pointer-events: none; text-decoration: line-through; padding-left: 5px; cursor: pointer;';
 function addTaskToPending(event)
 {
+    debugger;
+   // let tsk = document.getElementById('addTask').createElement(table, [tr]);
+}
+function doneTask(checkboxElem) { //! Strike through the task that has been done [css-- text-decoration: line-through]
+    debugger;
+    let taskComplete = $('.cbStrike:checked').val();
+    console.log(taskComplete);
+    if (checkboxElem.checked) {
+    var taskStrike = 'pointer-events: none; text-decoration: line-through; padding-left: 5px; cursor: pointer;';
+    } else {
+    var taskStrike = 'pointer-events: none; padding-left: 5px; cursor: pointer;';
+    }
+}
+function editTask(event) {//! Edit the task in line [NO MODAL]
+    debugger;
+}
+function deleteTask(event) {//! Delete the task and move the task count decriment 1
     debugger; 
-    console.log('this ');
- let test = document.getElementById('inputTask').console.log('this is log of iputTask');
-
-    
 }
